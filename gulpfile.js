f882015d5c26def1f65cc0b75629f8eaeb5fdd34
Lauren <laurenekcoker@gmail.com>
2018-07-06 14:17:08 -0500

    Completed setup for Github pages

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,30 @@ var gulp = require("gulp"),
     mixins = require("postcss-mixins"),
     watch = require("gulp-watch"),
     browserSync = require("browser-sync").create(),
-    modernizr = require("modernizr"),
     webpack = require("webpack"),
-    imagemin = require("gulp-imagemin"),
-    del = require("del"),
-    usemin = require("gulp-usemin"),
-    rev = require("gulp-rev"),
-    cssnano = require("gulp-cssnano"),
-    uglify = require("gulp-uglify");
+    imagemin = require("gulp-imagemin"), //Comparesses images
+    del = require("del"), //Deletes folders
+    usemin = require("gulp-usemin"), //Overarching compression package
+    rev = require("gulp-rev"), //Helps revision files
+    cssnano = require("gulp-cssnano"), //Compresses CSS
+    uglify = require("gulp-uglify"); //Compresses JS
 
 gulp.task("default", ["watch"]);
 
-gulp.task("watch", ["styles"], function(){
+gulp.task("watch", ["styles"], () => {
     browserSync.init({
         server: "./app"
     });
 
     gulp.watch("./app/assets/styles/**/*.css", ["styles"]);
-    gulp.watch("./app/assets/scripts/**/*.js", function(){
+    gulp.watch("./app/assets/scripts/**/*.js", () => {
         gulp.start("scriptsRefresh");
     });
     gulp.watch("./app/*.html").on("change", browserSync.reload);
 });
 
-gulp.task("scripts", function(callback){
-    webpack(require("./webpack.config.js"), function(err, stats){
+gulp.task("scripts", (callback) => {
+    webpack(require("./webpack.config.js"), (err, stats) => {
         if (err) {
             console.log(err.toString());
         }
@@ -40,13 +39,59 @@ gulp.task("scripts", function(callback){
     });
 });
 
-gulp.task("styles", function(){
+gulp.task("styles", () => {
     return gulp.src("./app/assets/styles/styles.css")
         .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest("./app/temp/styles"))
         .pipe(browserSync.stream());
 });
 
-gulp.task("scriptsRefresh", ["scripts"], function(){
+gulp.task("scriptsRefresh", ["scripts"], () => {
     browserSync.reload();
+});
+
+gulp.task("deleteDistFolder", () => {
+    return del("./docs");
+});
+
+gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], () => {
+    return gulp.src("./app/index.html")
+        .pipe(usemin({
+            css: [() =>  rev(), () =>  cssnano()],
+            js: [() => rev(), () => uglify()]
+        }))
+        .pipe(gulp.dest("./docs"));
+});
+
+gulp.task('copyGeneralFiles', ['deleteDistFolder'], () => {
+    const pathsToCopy = [
+        './app/**/*',
+        '!./app/index.html',
+        '!./app/assets/images/**',
+        '!./app/assets/styles/**',
+        '!./app/assets/scripts/**',
+        '!./app/temp',
+        '!./app/temp/**'
+    ];
+
+    return gulp.src(pathsToCopy)
+        .pipe(gulp.dest("./docs"));
+});
+
+gulp.task("optimizeImages", ['deleteDistFolder'], () => {
+    return gulp.src("./app/assets/images/**/*")
+        .pipe(imagemin({
+            progressive: true,
+            interlaced: true,
+            multipass: true
+        }))
+        .pipe(gulp.dest("./dist/assets/images"));
+});
+
+gulp.task("build", ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
+
+gulp.task("previewDist", () => {
+    browserSync.init({
+        server: "./docs"
+    });
 })
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ var path = require("path"),
 
 module.exports = {
     entry: {
-        App: "./app/assets/scripts/app.js",
-        Vendor: "./app/assets/scripts/vendor.js"
+        App: "./app/assets/scripts/app.js"
+        // Vendor: "./app/assets/scripts/vendor.js"
     },
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"),
